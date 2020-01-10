9f325690c67be37d50e2ee7e5e50c8609ec52f45
geekjoker-joy <geekjoker@foxmail.com>
2020-01-10 19:18:10 +0800

    vue-cil3 和 bootstrap4 搭建完成

---
+++ src/views/IndexManagement.vue
@@ -0,0 +1,14 @@
+<template>
+  <div  class="container-fluid">
+   <NavManagement/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+import NavManagement from "../components/NavManagement";
+export default {
+  components: {NavManagement}
+}
+</script>
